4968412c3b0aa5c6896e3ff7b59867c6491d7a5f
Prashant Pol <developer.prashant.pol@gmail.com>
2017-10-09 11:26:47 +0900

    gif added

--- app/src/main/java/com/polstech/library/guideddashpath/MainActivity.java
+++ app/src/main/java/com/polstech/library/guideddashpath/MainActivity.java
@@ -1,11 +1,9 @@
 package com.polstech.library.guideddashpath;
 
-import android.graphics.Rect;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.polstech.library.view.GuidedView;
 
@@ -16,16 +14,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final ImageView image1 = (ImageView) findViewById(R.id.img1);
-        final ImageView image2 = (ImageView) findViewById(R.id.img2);
-        final ImageView image3 = (ImageView) findViewById(R.id.img3);
-        final ImageView image4 = (ImageView) findViewById(R.id.img4);
+        TextView image1 = (TextView) findViewById(R.id.img1);
+        TextView image2 = (TextView) findViewById(R.id.img2);
+        TextView image3 = (TextView) findViewById(R.id.img3);
+        TextView image4 = (TextView) findViewById(R.id.img4);
+        TextView image5 = (TextView) findViewById(R.id.img5);
 
         final GuidedView guidedView = (GuidedView) findViewById(R.id.gv);
         guidedView.with(this)
                 .addViewPair(image1, image2)
                 .addViewPair(image2, image3)
                 .addViewPair(image3, image4)
+                .addViewPair(image4, image5)
                 .create();
     }
 }
